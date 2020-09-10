add Color Detector Fix HistogramBug Fix scaling when using multi DPI

py4DSTEM/gui/viewer.py
@@ -53,6 +53,7 @@ class DataViewer(QtWidgets.QMainWindow):
         # flag
         self.updating_roi = False
 
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
         # Define this as the QApplication object
         self.qtapp = QtWidgets.QApplication.instance()
         if not self.qtapp:
@@ -121,9 +122,9 @@ class DataViewer(QtWidgets.QMainWindow):
 
 
         if diffractionSpace:
-            detector.rois[0].sigRegionChangeFinished.connect(self.update_real_space_view)
+            detector.rois[0].sigRegionChangeFinished.connect(lambda: self.update_real_space_view(False))
         else:
-            detector.rois[0].sigRegionChangeFinished.connect(self.update_diffraction_space_view)
+            detector.rois[0].sigRegionChangeFinished.connect(lambda: self.update_diffraction_space_view(False))
 
         # Treat Annular Circle
         if detector.shape_type == cs.DetectorShape.annular:
@@ -143,13 +144,13 @@ class DataViewer(QtWidgets.QMainWindow):
 
     def select(self, detector: detector.Detector):
         if detector.selected:
-            detector.unselect()
+            detector.unselectEvent()
             delattr(self, 'selected_detector')
         else:
             if hasattr(self, 'selected_detector'):
-                self.selected_detector.unselect()
+                self.selected_detector.unselectEvent()
             self.selected_detector = detector
-            detector.select()
+            detector.selectEvent()
 
 
     dtt:detector.Detector
@@ -164,7 +165,7 @@ class DataViewer(QtWidgets.QMainWindow):
             lambda: detector.controlWidget.frame.setStyleSheet("QFrame#frame{border: 3px solid #ffff00;}") if not detector.selected else 0)
 
         detector.controlWidget.addLeaveEvent(
-            lambda: detector.rois[0].setPen(color='g') if not detector.selected else 0)
+            lambda: detector.rois[0].setPen(color=detector.color) if not detector.selected else 0)
         detector.controlWidget.addLeaveEvent(
             lambda: detector.controlWidget.frame.setStyleSheet("QFrame#frame{border: 3px solid #444a4f;}") if not detector.selected else 0)
 
@@ -269,6 +270,13 @@ class DataViewer(QtWidgets.QMainWindow):
         self.settings.New('real_scaling_mode', dtype=int, initial=0)
         self.settings.real_scaling_mode.connect_bidir_to_widget(self.control_widget.buttonGroup_realMode)
         self.settings.real_scaling_mode.updated_value.connect(self.real_sacling_changed)
+
+        ## Setting ##
+        self.settings.New('color_mode', dtype=bool, initial=False)
+        self.settings.color_mode.connect_bidir_to_widget(self.control_widget.checkBox_color)
+        self.settings.color_mode.updated_value.connect(self.update_color_checked)
+        self.settings.New('real_time_update_mode', dtype=bool, initial=False)
+        self.settings.real_time_update_mode.connect_bidir_to_widget(self.control_widget.checkBox_update)
 
         ## Virtual Detector Shape ##
         self.control_widget.pushBtn_rect_diffractionSpace.clicked.connect(
@@ -464,10 +472,11 @@ class DataViewer(QtWidgets.QMainWindow):
             self.datacube,
             [mk.RoiMask((slice(0, 1), slice(0, 1)), roiShape=cs.DetectorShape.point)]
         )
-        self.diffraction_space_widget.setImage(self.diffraction_space_view, autoLevels=False, autoRange=False)
+        self.diffraction_space_widget.setImage(self.diffraction_space_view, autoLevels=True, autoRange=True)
 
         # self.update_virtual_detector_mode()
         self.addShape_diffractionSpace(cs.DetectorShape.rectangular)
+        self.real_space_widget.autoLevels()
         self.real_space_point_selector = self.addShape_realSpace(cs.DetectorShape.point)
 
         # Normalize diffraction space view
@@ -478,8 +487,7 @@ class DataViewer(QtWidgets.QMainWindow):
         self.control_widget.spinBox_Nx.setMaximum(self.datacube.R_N)
         self.control_widget.spinBox_Ny.setMaximum(self.datacube.R_N)
 
-        self.real_space_widget.autoRange()
-        self.diffraction_space_widget.autoRange()
+        self.real_space_widget.levelMin = 0
 
         # titleBar
         self.titleBar.addRecentFile(fname)
@@ -766,35 +774,75 @@ class DataViewer(QtWidgets.QMainWindow):
     ################## Get virtual images ##################
 
     def diffraction_scaling_changed(self):
-        self.update_diffraction_space_view()
-        self.diffraction_space_widget.autoLevels()
+        self.update_diffraction_space_view(True)
 
     def real_sacling_changed(self):
-        self.update_real_space_view()
-        self.real_space_widget.autoLevels()
+        self.update_real_space_view(True)
 
-    def update_diffraction_space_view(self):
+    def update_color_checked(self):
+        # if self.control_widget.checkBox_color.isChecked():
+        #     for detector in self.detectorGroup_diffractionSpace:
+        #         detector.controlWidget.colorButton.setVisible(True)
+        #     for detector in self.detectorGroup_realSpace:
+        #         detector.controlWidget.colorButton.setVisible(True)
+        # else:
+        #     for detector in self.detectorGroup_diffractionSpace:
+        #         detector.controlWidget.colorButton.setVisible(False)
+        #     for detector in self.detectorGroup_realSpace:
+        #         detector.controlWidget.colorButton.setVisible(False)
+        if self.control_widget.checkBox_color.isChecked():
+            self.control_widget.radioButton_DiffX.setDisabled(True)
+            self.control_widget.radioButton_DiffY.setDisabled(True)
+            self.control_widget.radioButton_CoMX.setDisabled(True)
+            self.control_widget.radioButton_CoMY.setDisabled(True)
+            # self.diffraction_space_widget.getHistogramWidget().item.setLevelMode('rgba')
+            # self.real_space_widget.getHistogramWidget().item.setLevelMode('rgba')
+
+        else:
+            self.control_widget.radioButton_DiffX.setDisabled(False)
+            self.control_widget.radioButton_DiffY.setDisabled(False)
+            self.control_widget.radioButton_CoMX.setDisabled(False)
+            self.control_widget.radioButton_CoMY.setDisabled(False)
+            # self.diffraction_space_widget.getHistogramWidget().item.setLevelMode('mono')
+            # self.real_space_widget.getHistogramWidget().item.setLevelMode('mono')
+        if not self.control_widget.radioButton_Integrate.isChecked():
+            self.control_widget.radioButton_Integrate.setChecked(True)
+
+        # self.diffraction_space_widget.getHistogramWidget().item.vb.setLimits(yMin=1)
+        # self.real_space_widget.getHistogramWidget().item.vb.setLimits(yMin=1)
+
+        self.update_real_space_view(autoLevels=True)
+        self.update_diffraction_space_view(autoLevels=True)
+
+        # todo
+        # self.diffraction_space_widget.getHistogramWidget().item.vb.
+        # print(self.diffraction_space_widget.getHistogramWidget().item.getLevels())
+
+    def update_diffraction_space_view(self, autoLevels=False):
         if len(self.detectorGroup_realSpace) == 0:
             return
 
-        ## Create Mask ##
-        roi_mask_grp = mk.get_mask_grp_from_rois(self.detectorGroup_realSpace,
-                                                 self.real_space_widget.getImageItem(),
-                                                 True)
+        if self.settings.color_mode.val == False:
+            ## Create Mask ##
+            roi_mask_grp = mk.get_mask_grp_from_rois(self.detectorGroup_realSpace,
+                                                     self.real_space_widget.getImageItem(),
+                                                     True)
+            ## Get Virtual Image ##
+            new_diffraction_space_view, success = compute.get_diffraction_image(self.datacube, masks=roi_mask_grp)
 
-        ## Get Virtual Image ##
-        new_diffraction_space_view, success = compute.get_diffraction_image(self.datacube, masks=roi_mask_grp)
+        else:
+            new_diffraction_space_view, success = compute.get_diffraction_image_color(self.datacube,
+                                                                           self.detectorGroup_realSpace)
 
-        ## Set Image ##
         if success:
             # Scaling
             self.diffraction_space_view = compute.scaling(new_diffraction_space_view, self.settings.diffraction_scaling_mode.val, self.datacube)
             self.diffraction_space_widget.setImage(self.diffraction_space_view,
-                                                   autoLevels=False, autoRange=False)
+                                                   autoLevels=autoLevels)
 
-        else:
-            pass
-        return
+        ## Scale Level For Color ##
+        if autoLevels == True and self.settings.color_mode.val == True:
+            self.diffraction_space_widget.setLevels(min=self.diffraction_space_view.min(), max=self.diffraction_space_view.max())
 
     def update_diffraction_space_view2(self):
         roi_state = self.real_space_point_selector.saveState()
@@ -826,10 +874,6 @@ class DataViewer(QtWidgets.QMainWindow):
         else:
             pass
         return
-
-    def getSlices(self, roi: pg.ROI) -> tuple:
-        _slices, _transforms = roi.getArraySlice(self.datacube.data[0, 0, :, :],self.diffraction_space_widget.getImageItem())
-        return _slices
 
     # def update_dialog_diffractionSpace(self):
     #
@@ -947,7 +991,7 @@ class DataViewer(QtWidgets.QMainWindow):
     #     self.updating_roi = False
 
 
-    def update_real_space_view(self):
+    def update_real_space_view(self, autoLevels=False):
         tic = time.process_time()
         virtual_detector_mode = self.settings.virtual_detector_mode.val
 
@@ -955,33 +999,36 @@ class DataViewer(QtWidgets.QMainWindow):
         if len(self.detectorGroup_diffractionSpace) == 0:
             return
 
-        ## Create Mask ##
-        roi_mask_grp = mk.get_mask_grp_from_rois(self.detectorGroup_diffractionSpace,
-                                                 self.diffraction_space_widget.getImageItem(),
-                                                 False)
+        if self.settings.color_mode.val == False:
+            ## Create Mask ##
+            roi_mask_grp = mk.get_mask_grp_from_rois(self.detectorGroup_diffractionSpace,
+                                                     self.diffraction_space_widget.getImageItem(),
+                                                     False)
 
-        ## Get Virtual Image ##
-        new_real_space_view, success = compute.get_virtual_image(self.datacube, masks=roi_mask_grp, integration_mode=virtual_detector_mode)
+            ## Get Virtual Image ##
+            new_real_space_view, success = compute.get_virtual_image(self.datacube, masks=roi_mask_grp, integration_mode=virtual_detector_mode)
+        else:
+            new_real_space_view, success = compute.get_virtual_image_color(self.datacube, self.detectorGroup_diffractionSpace)
 
-        ## Scaling ##
-        new_real_space_view = compute.scaling(new_real_space_view,
-                                                      self.settings.real_scaling_mode.val, self.datacube)
+        if success:
+            ## Scaling ##
+            new_real_space_view = compute.scaling(new_real_space_view,
+                                                  self.settings.real_scaling_mode.val, self.datacube)
 
-        # # Color Test
-        # new_real_space_view = np.dstack((new_real_space_view, np.zeros(new_real_space_view.shape),
-        #                                       np.zeros(new_real_space_view.shape)))
+            ## Set Image ##
+            self.real_space_view = new_real_space_view
+            self.real_space_widget.setImage(self.real_space_view, autoLevels=autoLevels)
 
-        ## Set Image ##
-        self.real_space_view = new_real_space_view
+            ## Scale Level For Color ##
+            if autoLevels == True and self.settings.color_mode.val == True:
+                self.real_space_widget.setLevels(min=self.real_space_view.min(), max=self.real_space_view.max())
 
-        self.real_space_widget.setImage(self.real_space_view, autoLevels=True)
+            ## Update strain_window ##
+            if self.strain_window is not None:
+                self.strain_window.bragg_disk_tab.update_views()
 
-        ## Update strain_window ##
-        if self.strain_window is not None:
-            self.strain_window.bragg_disk_tab.update_views()
-
-        toc = time.process_time()
-        print("analysis done in "+str(toc-tic)+"ms")
+            toc = time.process_time()
+            print("analysis done in "+str(toc-tic)+"ms")
 
 
     def exec_(self):
@@ -1002,13 +1049,13 @@ class QtWidget_Key(QtWidgets.QWidget):
             roi_state = self.dataViewer.selected_detector.rois[0].saveState()
             x0, y0 = roi_state['pos']
             if e.key() == QtCore.Qt.Key_Left:
-                x0 = (x0-1)%(self.dataViewer.datacube.data.shape[0])
+                x0 = (x0-1)%(self.dataViewer.selected_detector.imageView.image.shape[0])
             elif e.key() == QtCore.Qt.Key_Right:
-                x0 = (x0+1)%(self.dataViewer.datacube.data.shape[0])
+                x0 = (x0+1)%(self.dataViewer.selected_detector.imageView.image.shape[0])
             elif e.key() == QtCore.Qt.Key_Up:
-                y0 = (y0-1)%(self.dataViewer.datacube.data.shape[1])
+                y0 = (y0-1)%(self.dataViewer.selected_detector.imageView.image.shape[1])
             elif e.key() == QtCore.Qt.Key_Down:
-                y0 = (y0+1)%(self.dataViewer.datacube.data.shape[1])
+                y0 = (y0+1)%(self.dataViewer.selected_detector.imageView.image.shape[1])
             self.dataViewer.selected_detector.rois[0].setPos((x0,y0))
 
 

py4DSTEM/process/imaging/compute.py
@@ -25,10 +25,24 @@ def get_virtual_image(datacube: datacube, masks: list, integration_mode: cs.Dete
                 rs += img
     return rs, success
 
-def get_virtual_image_color(datacube: datacube, detectors: list, integration_mode: cs.DetectorModeType,diffractionSpace):
-    # mask = detector.create_roi_mask()
-    #
-    pass
+def get_virtual_image_color(datacube: datacube, detectors: list):
+    integrated_img = None
+    for detector in detectors:
+        mask = detector.create_roi_mask()
+        mask.trim_data_to_image()
+        img, rs = vp.get_virtual_image(datacube, 0, mask, None)
+        if rs == 1:
+            img_r = img * detector.color_r / 255
+            img_g = img * detector.color_g / 255
+            img_b = img * detector.color_b / 255
+            img = np.dstack((img_r, img_g, img_b))
+            if integrated_img is None:
+                integrated_img = img
+            else :
+                integrated_img += img
+        else:
+            return 0,0
+    return integrated_img, 1
 
 
 def get_diffraction_image(datacube: datacube, masks: list):
@@ -57,6 +71,30 @@ def get_diffraction_image(datacube: datacube, masks: list):
     return rs, success
 
 
+def get_diffraction_image_color(datacube: datacube, detectors: list):
+    integrated_img = None
+    pixel_count = 0
+    for detector in detectors:
+        mask = detector.create_roi_mask()
+        mask.trim_data_to_image()
+        img, rs = di.get_diffraction_image(datacube,mask)
+        pixel_count += np.sum(mask.data)
+        if rs == 1:
+            img_r = img * detector.color_r / 255
+            img_g = img * detector.color_g / 255
+            img_b = img * detector.color_b / 255
+            img = np.dstack((img_r, img_g, img_b))
+            if integrated_img is None:
+                integrated_img = img
+            else:
+                integrated_img += img
+        else:
+            return 0, 0
+    if pixel_count != 0:
+        img = img / pixel_count
+    return integrated_img, 1
+
+
 def scaling(img, mode, datacube: datacube):
     if mode == 1:
         # sqrt mode
@@ -68,6 +106,10 @@ def scaling(img, mode, datacube: datacube):
     elif mode == 3:
         # EWPC mode
         h = np.hanning(datacube.Q_Nx)[:, np.newaxis] @ np.hanning(datacube.Q_Ny)[np.newaxis, :]
-        img = np.abs(np.fft.fftshift(np.fft.fft2(np.log(
-            (h * (img - np.min(img))) + 1)))) ** 2
+        if len(img.shape) == 2:
+            img = np.abs(np.fft.fftshift(np.fft.fft2(np.log(
+                (h * (img - np.min(img))) + 1)))) ** 2
+        elif len(img.shape) == 3:
+            img = np.abs(np.fft.fftshift(np.fft.fft2(np.log(
+                (np.dstack((h,h,h)) * (img - np.min(img))) + 1)))) ** 2
     return img

py4DSTEM/process/imaging/compute_real_image.py
@@ -36,7 +36,7 @@ def get_virtual_image_diffX(datacube: datacube, mask: mk.RoiMask, center: tuple)
     """
     try:
 
-        if center is not None:
+        if center is None:
             center_x, center_y = mask.getCenter()
         else:
             center_x, center_y = center
@@ -47,7 +47,7 @@ def get_virtual_image_diffX(datacube: datacube, mask: mk.RoiMask, center: tuple)
             img = np.ndarray.astype(
                 np.sum(datacube.data[:, :, slice_left, mask.slice_y], axis=(2, 3)) -
                 np.sum(datacube.data[:, :, slice_right, mask.slice_y], axis=(2, 3)),
-                'int64'
+                'float64'
             )
         else:
             img = np.ndarray.astype(
@@ -57,7 +57,7 @@ def get_virtual_image_diffX(datacube: datacube, mask: mk.RoiMask, center: tuple)
                 np.sum(datacube.data[:, :, slice_right, mask.slice_y] * mask.data[slice_right.start - slice_right.stop:,
                                                                         :],
                        axis=(2, 3)),
-                'int64')
+                'float64')
         return img, 1
     except ValueError:
         return 0, 0
@@ -81,7 +81,7 @@ def get_virtual_image_diffY(datacube: datacube, mask: mk.RoiMask, center: tuple)
             img = np.ndarray.astype(
                 np.sum(datacube.data[:, :, mask.slice_x, slice_left], axis=(2, 3)) -
                 np.sum(datacube.data[:, :, mask.slice_x, slice_right], axis=(2, 3)),
-                'int64'
+                'float64'
             )
         else:
             img = np.ndarray.astype(
@@ -90,7 +90,7 @@ def get_virtual_image_diffY(datacube: datacube, mask: mk.RoiMask, center: tuple)
                 np.sum(datacube.data[:, :, mask.slice_x, slice_right] * mask.data[:,
                                                                         slice_right.start - slice_right.stop:],
                        axis=(2, 3)),
-                'int64')
+                'float64')
         return img, 1
     except ValueError:
         return 0, 0

py4DSTEM/process/imaging/mask.py
@@ -8,7 +8,10 @@ class RoiMask():
 
     def __init__(self, slices: tuple, roiShape=None, innerR=0, data=None, maxSize:tuple=None):
 
-        self.slice_x, self.slice_y = slices
+        if len(slices) == 2:
+            self.slice_x, self.slice_y = slices
+        elif len(slices) == 3:
+            self.slice_x, self.slice_y, _ = slices
         self.size_x = self.slice_x.stop - self.slice_x.start
         self.size_y = self.slice_y.stop - self.slice_y.start
         self.innerR = innerR
@@ -42,6 +45,9 @@ class RoiMask():
             self.isMerged = True
 
     def trim_data_to_image(self):
+        """
+        crop mask when get out of image boundary
+        """
         if self.maxSize is not None:
             max_x, max_y = self.maxSize
             x_start = self.slice_x.start
@@ -79,12 +85,6 @@ class RoiMask():
         center_x = int(np.sum(grid_x * self.data) / np.sum(self.data))
         center_y = int(np.sum(grid_y * self.data) / np.sum(self.data))
         return center_x, center_y
-
-    def crop_to_fit_image_size(self):
-        """
-        crop mask when get out of image boundary
-        """
-        # todo
 
     def count_mask_pixel(self):
         return np.sum(self.data)
@@ -167,35 +167,31 @@ def get_compound_mask_list(mask_list: list):
     return compound_mask
 
 
-def get_mask_grp_from_rois(detector_grp: list, diffractionImageItem: pg.ImageView, diffractionSpace=True):
+def get_mask_grp_from_rois(detector_grp: list, imageView: pg.ImageView, diffractionSpace=True):
 
     roi_mask_grp = []
     for dtt in detector_grp:
         shape_type = dtt.shape_type
         rois = dtt.rois
 
-        if diffractionSpace:
-            slices, transforms = dtt.rois[0].getArraySlice(diffractionImageItem.image,
-                                                           diffractionImageItem)
-        else:
-            slices, transforms = dtt.rois[0].getArraySlice(diffractionImageItem.image,
-                                                           diffractionImageItem)
+        slices, transforms = dtt.rois[0].getArraySlice(imageView.image,
+                                                       imageView)
 
         if shape_type in (cs.DetectorShape.rectangular, cs.DetectorShape.circular):
-            mask = RoiMask(roiShape=shape_type, slices=slices, maxSize=diffractionImageItem.image.shape)
+            mask = RoiMask(roiShape=shape_type, slices=slices, maxSize=imageView.image.shape[0:2])
         elif shape_type is cs.DetectorShape.point:
             x = np.int(np.ceil(rois[0].x()))
             y = np.int(np.ceil(rois[0].y()))
             slices = (slice(x, x + 1), slice(y, y + 1))
-            mask = RoiMask(roiShape=shape_type, slices=slices, maxSize=diffractionImageItem.image.shape)
+            mask = RoiMask(roiShape=shape_type, slices=slices, maxSize=imageView.image.shape[0:2])
         elif shape_type is cs.DetectorShape.annular:
             slice_x, slice_y = slices
-            slices_inner, transforms = rois[1].getArraySlice(diffractionImageItem.image,
-                                                            diffractionImageItem)
+            slices_inner, transforms = rois[1].getArraySlice(imageView.image,
+                                                             imageView)
             slice_inner_x, slice_inner_y = slices_inner
             R = 0.5 * ((slice_inner_x.stop - slice_inner_x.start) / (slice_x.stop - slice_x.start) + (
                     slice_inner_y.stop - slice_inner_y.start) / (slice_y.stop - slice_y.start))
-            mask = RoiMask(roiShape=shape_type, slices=slices, innerR=R, maxSize=diffractionImageItem.image.shape)
+            mask = RoiMask(roiShape=shape_type, slices=slices, innerR=R, maxSize=imageView.image.shape[0:2])
         roi_mask_grp.append(mask)
     return roi_mask_grp
 